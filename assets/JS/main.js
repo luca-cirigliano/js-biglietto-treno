81c162a4989b19da7b90ec08d0f2ae8a42786349
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-09 15:45:32 +0100

    exercise complete

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,5 @@
 /* 
-
 context: 
-
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -11,7 +9,6 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 Questo richiederà un minimo di ricerca.
 
 */
-
 /* lista strumenti
 
 - prompt
@@ -19,35 +16,62 @@ Questo richiederà un minimo di ricerca.
 - let
 - console.log
 - if else
--number
+- number/parseInt
+- getElemetById
+- toFixed()
 */
-
 // Chiedo all'utente quanti km deve percorrere e controllo che sia un numero 
 
 const km = Number(prompt('quanti km devi percorrere?'));
-console.log(`km = ${km}`)
 
 //e che sia maggiore di 0
+
 if (isNaN(km) || km <= 0) {
     alert("ERRORE: Il valore deve essere un numero maggiore di 0.");
   }
 
 // Chiedo all'utente l'età e controllo che sia un numero 
-const userAge = Number(prompt('quanti anni hai?'));
-console.log(`age = ${userAge}`)
 
+const userAge = Number(prompt('quanti anni hai?'));
 
 //e che sia maggiore o uguale a 0
+
 if (isNaN(userAge) || userAge <= 0) {
     alert("ERRORE: Il valore deve essere un numero maggiore di 0.");
   }
+console.log(`age = ${userAge} km = ${km}`);
 
 // Calcolo il costo totale del viaggio
-const totalAmount = km * 0.21
-console.log(`total amount = ${totalAmount}`)
-// Calcolo lo sconto per minorenni e over65
 
-// Formatto il prezzo scontato per avere due cifre decimali
+const price_per_km = 0.21
+let ticket_price = km * price_per_km;
+console.log(ticket_price, 'Prezzo intero');
 
-// Mostro all'utente il prezzo finale in pagina
-document.getElementById("total_amount").innerHTML =`${totalAmount}`;
+// Calcolo lo sconto per minorenni 20%
+// Calcolo lo sconto per over65 40%
+
+let discount = 0
+if(userAge < 18) {
+  discount = 0.2
+} else if(userAge > 65) {
+  discount = 0.4
+}
+console.log(discount, 'discount');
+
+const discountAmount = ticket_price * discount
+console.log(discountAmount, 'discount amount');
+
+ticket_price = ticket_price - discountAmount
+
+console.log(ticket_price, 'prezzo scontato');
+
+if (isNaN(ticket_price)) {
+  alert('Ciao, devi inserire nei campi dei numeri e non dei testi')
+  // mopstro a schermo un messaggio di errore al posto del prezzo
+  priceElement.innerHTML = `ERRORE: Il valore deve essere un numero maggiore di 0.`
+} 
+else{
+  // Formatto il prezzo scontato per avere due cifre decimali
+  // Mostro all'utente il prezzo finale in pagina
+  document.getElementById("total_amount").innerHTML =`€ ${ticket_price.toFixed(2)}`;
+}
